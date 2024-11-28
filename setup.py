3964c95724655aed52fe7d6b4eac98757090e17c
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2024-11-28 10:26:57 -0600

    Add tests

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
+import os
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
+
+linetrace = os.environ.get("LINETRACE", False) == "True"
 
 ext_modules = [
     Extension(
@@ -10,6 +13,9 @@ ext_modules = [
 ]
 
 setup(
-    ext_modules=cythonize(ext_modules),
+    ext_modules=cythonize(
+        ext_modules,
+        compiler_directives={"language_level": "3", "linetrace": linetrace},
+    ),
     include_dirs=[np.get_include()],
 )
---
+++ tests/tests_basic.py
@@ -0,0 +1,50 @@
+import os
+import shutil
+
+import pytest
+from click.testing import CliRunner
+from pyempad_calibrate.command import main
+
+
+@pytest.fixture
+def test_files_path(tmp_path):
+    src_path = "/home/xarth/codes/xarthisius/pyempadcalibratescript/related_data/EMPAD2-calib_oct2020"
+    data_path = tmp_path / "data"
+    data_path.mkdir()
+    background_path = data_path / "background.bin"
+    shutil.copy(os.path.join(src_path, "bkgd_reduced.raw"), background_path)
+
+    raw_path = data_path / "raw.bin"
+    shutil.copy(os.path.join(src_path, "raw_reduced.raw"), raw_path)
+
+    calib_path = tmp_path / "calib"
+    calib_path.mkdir()
+    for file in os.listdir(src_path):
+        if file.endswith(".r32"):
+            shutil.copy(os.path.join(src_path, file), calib_path)
+
+    return tmp_path
+
+
+def test_main(test_files_path):
+    runner = CliRunner()
+    with runner.isolated_filesystem(temp_dir=test_files_path) as td:
+        result = runner.invoke(
+            main,
+            [
+                "--calib_path",
+                os.path.join(td, "..", "calib"),
+                "--shape",
+                "128",
+                "128",
+                "--output_path",
+                ".",
+                os.path.join(td, "..", "data/background.bin"),
+                os.path.join(td, "..", "data/raw.bin"),
+            ],
+        )
+        assert result.exit_code == 0
+        assert "Background shape: (128, 128, 10000)" in result.output
+        assert "Raw data shape: (128, 128, 10000)" in result.output
+        assert "Debouncing" in result.output
+        assert "Multiplying by flat fields" in result.output
