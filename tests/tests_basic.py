c2211972367afdb36a3fb8e2400e093c54d3c2fb
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2024-11-28 10:36:19 -0600

    Fix test

--- tests/tests_basic.py
+++ tests/tests_basic.py
@@ -8,14 +8,14 @@ from pyempad_calibrate.command import main
 
 @pytest.fixture
 def test_files_path(tmp_path):
-    src_path = "/home/xarth/codes/xarthisius/pyempadcalibratescript/related_data/EMPAD2-calib_oct2020"
+    src_path = os.path.join(os.path.dirname(__file__), "data")
     data_path = tmp_path / "data"
     data_path.mkdir()
     background_path = data_path / "background.bin"
-    shutil.copy(os.path.join(src_path, "bkgd_reduced.raw"), background_path)
+    shutil.copy(os.path.join(src_path, "bkgd", "n1000.bkgd"), background_path)
 
     raw_path = data_path / "raw.bin"
-    shutil.copy(os.path.join(src_path, "raw_reduced.raw"), raw_path)
+    shutil.copy(os.path.join(src_path, "raw", "n1000.raw"), raw_path)
 
     calib_path = tmp_path / "calib"
     calib_path.mkdir()
@@ -44,7 +44,7 @@ def test_main(test_files_path):
             ],
         )
         assert result.exit_code == 0
-        assert "Background shape: (128, 128, 10000)" in result.output
-        assert "Raw data shape: (128, 128, 10000)" in result.output
+        assert "Background shape: (128, 128, 1000)" in result.output
+        assert "Raw data shape: (128, 128, 1000)" in result.output
         assert "Debouncing" in result.output
         assert "Multiplying by flat fields" in result.output
